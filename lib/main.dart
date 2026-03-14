97cf73ee6beed1ae55a65c84309d2c8a24a35cf4
isopha2003@gmail.com <isopha2003@gmail.com>
2026-03-14 21:23:29 +0900

    앱 진입점 설정 (Hive 초기화, Riverpod 설정, 메인 화면 연결)

---
+++ lib/main.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:hive_flutter/hive_flutter.dart';
+import 'screens/forest_screen.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Hive.initFlutter();
+
+  runApp(
+    const ProviderScope(
+      child: MyApp(),
+    ),
+  );
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: '잠든 사이 숲',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(
+          seedColor: const Color(0xFF1B4332),
+        ),
+        useMaterial3: true,
+      ),
+      home: const ForestScreen(),
+    );
+  }
+}
