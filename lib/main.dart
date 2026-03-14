8c7666a8899a18b4c05cbcd35d9f5823c5bbe3fe
isopha2003@gmail.com <isopha2003@gmail.com>
2026-03-14 23:04:17 +0900

    푸시 알림 서비스 추가 (동물 방문, 나무 성장, 이슬 알림)

--- lib/main.dart
+++ lib/main.dart
@@ -2,10 +2,12 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:hive_flutter/hive_flutter.dart';
 import 'screens/forest_screen.dart';
+import 'services/notification_service.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Hive.initFlutter();
+  await NotificationService.init();
 
   runApp(
     const ProviderScope(
